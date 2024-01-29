5547311ef939fef22421176bcf2171a1db01b855
Kanay <kanosss1@gmail.com>
2024-01-29 12:09:29 +0600

    Bayel uktaba

---
+++ main.js
@@ -0,0 +1,97 @@
+// ! Переменные
+
+// ! Команды для создание переменных
+// ? var -  имеет глобальную область видимости, не используеться сейчас но 80% вебсайтов сделано при помощи var. Имеет проблему с хойстингом(hoising)
+// ? const - неизменяемые переменные
+// ? let - изменяемое переменное
+
+// ! camelCase bayelKrasav4ikBala
+
+// let studentName = "Abdulaziz";
+// console.log(studentName);
+// studentName = "Chakraborti";
+// console.log(studentName);
+
+// const studentAge = 16;
+// console.log(studentAge);
+// studentAge = 53;
+// console.log(studentAge);
+
+// ! Типы данных
+
+// ! number  Число      let salary = 1000
+// ! string  Текст      let model = "Passat"
+// ! boolean Условность     let isHandsome = false
+// ! null    Специальная пустота let coupleName = null
+// ! symbol  Идентификатор  let plane = Symbol('34')
+// ! undefined Пустота      let promise;
+// ! bigInt  Большое число  let spaceNumber = e67890988765970
+// ! object Объект со свойствами        let student = {name: "Bahonty", age: 75}
+
+// let age = 119;
+// let name = "Stalin";
+// let isDie = true;
+
+// let person = {
+//   name: "Pushkin",
+//   age: 95,
+//   isDie: true,
+//   isHandsome: false,
+// };
+
+// console.log(age);
+// console.log(name);
+// console.log(isDie);
+// console.log(person);
+
+// ! {} - фигурные скобки. Для объектов, когда пишем js код
+// ! [] - квадратные скобкию. Для массивов, индексов
+// ! () - круглые скобки. Для вызова функций, для аргументов
+
+let poet = "Aitmatov";
+let age = 95;
+
+console.log(poet, age);
+// 1 ball
+
+let num1 = "5";
+let num2 = 10;
+
+console.log(num1 + num2);
+// 1 ball
+
+let myFavoriteBook = "Harry Potter";
+let pagesInBook = 672;
+
+// 1 ball
+
+let student = {
+  name: "Japarov",
+  age: 321,
+};
+
+// 1 ball
+
+let motorola = null;
+let apple;
+
+console.log(motorola);
+console.log(typeof apple);
+
+// 1 ball
+
+let string = "string";
+let number = 2;
+let boolean = true;
+let students = {
+  name: "Japarov",
+  age: 321,
+};
+
+// 1 ballawdadadwadawwa 1 version
+
+// let
+// const
+// let
+
+// 2 ball   2 version
